ab08bd3917d4e0b8042a2915f068fde226a09c1e
KaroUniform <snow.leopard.111@yandex.ru>
2024-01-21 21:14:24 +0300

    🐛 Fix JWT

--- src/main_page.py
+++ src/main_page.py
@@ -1,10 +1,22 @@
 import streamlit as st
 import requests
+from streamlit_cookies_manager import EncryptedCookieManager
+
+
+cookies = EncryptedCookieManager(
+    password="My secret password",
+)
+if not cookies.ready():
+    # Wait for the component to load and send us current cookies.
+    st.stop()
+
 
 st.title("Malware Classification App")
 st.header("Login")
 
-st.write("Please login to have an access to a all functions. If you don't have an account go to register page and create one.")
+st.write(
+    "Please login to have an access to a all functions. If you don't have an account go to register page and create one."
+)
 
 email = st.text_input("Email")
 password = st.text_input("Password")
@@ -14,6 +26,13 @@ inputs = {
     "password": password,
 }
 
-if st.button('Submit'):
-    res = requests.post(url = "http://localhost:8000/auth/login", data=inputs)
-    st.subheader(f"Response from API * = {res.status_code}")
+if st.button("Submit"):
+    res = requests.post(url="http://localhost:8000/auth/login", data=inputs)
+    st.subheader(f"Response from API * = { res.status_code }")
+    if res.status_code == 204:
+        st.success("Login successful")
+        # Сохраняем куки в st.session_state
+        cookies["find_malwares"] = res.cookies.get("find_malwares")
+        cookies.save()
+    else:
+        st.error("Login failed")
--- src/pages/user_page.py
+++ src/pages/user_page.py
@@ -1,8 +1,22 @@
 import streamlit as st
 import requests
+from streamlit_cookies_manager import EncryptedCookieManager
+
+
+cookies = EncryptedCookieManager(
+    password="My secret password",
+)
+if not cookies.ready():
+    # Wait for the component to load and send us current cookies.
+    st.stop()
+
 
 st.title("Malware Classification App")
 st.header("Your account")
 
-res = requests.get(url = "http://localhost:8000/current_user")
+
+res = requests.get(
+    url="http://localhost:8000/current_user",
+    cookies={"find_malwares": cookies.get("find_malwares")},
+)
 st.subheader(f"Response from API * = {res.text}")
